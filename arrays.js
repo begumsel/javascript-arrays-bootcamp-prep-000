8eaf787c3ed5bf2e4a0d2738852f6186cb5ff9cd
Begum S. Tuyen <begumtuyen@gmail.com>
2019-01-05 22:43:19 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,7 @@ function addElementToBeginningOfArray(array, element){
 }
   
 function destructivelyAddElementToBeginningOfArray(array, element){
-  return ["element",...array] 
+  return ["foo",...array] 
 }
 
 function addElementToEndOfArray(array, element){
